cff12e0cb6f36b11c1f5417fe86157378cb3d7fa
Seongho Lee <tjdgh2626@gmail.com>
2023-10-16 10:09:23 +0900

    Add some activation functions

--- activation.py
+++ activation.py
@@ -11,10 +11,30 @@ def step(x: np.ndarray) -> np.ndarray:
 def step_diff(x: np.ndarray) -> np.ndarray:
     return np.ones(x.shape)
 
+# relu activation function
+def relu(x: np.ndarray) -> np.ndarray:
+    return np.where(x >= 0, x, 0)
+
+# relu activation differential
+def relu_diff(x: np.ndarray) -> np.ndarray:
+    return np.where(x >= 0, 1, 0)
+
+# sigmoid activation function
+def sigmoid(x: np.ndarray) -> np.ndarray:
+    return 1 / (1 + np.exp(-x))
+
+# sigmoid activation differential
+def sigmoid_diff(x: np.ndarray) -> np.ndarray:
+    return sigmoid(x) * (1 - sigmoid(x))
+
 # get activation function by name
 def get_activation(name: str):
     if name == 'step':
         return step
+    elif name == 'relu':
+        return relu
+    elif name == 'sigmoid':
+        return sigmoid
     else:
         raise Exception('Unknown activation function.')
     
@@ -22,5 +42,9 @@ def get_activation(name: str):
 def get_differential(name: str):
     if name == 'step':
         return step_diff
+    elif name == 'relu':
+        return relu_diff
+    elif name == 'sigmoid':
+        return sigmoid_diff
     else:
         raise Exception('Unknown activation function differential.')
